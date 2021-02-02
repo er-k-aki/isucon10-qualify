c6cacc8d96e90d5a93f6d8d58ac48d286e62e928
er-k-aki <akira.kosuda@ever-rise.co.jp>
2021-02-02 17:19:28 +0900

    fix

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -967,7 +967,6 @@ func searchEstateNazotte(c echo.Context) error {
 		return c.NoContent(http.StatusBadRequest)
 	}
 
-	b := coordinates.getBoundingBox()
 	//estatesInBoundingBox := []Estate{}
 	//query := `SELECT id, name, description, thumbnail, address, latitude, longitude, rent, door_height, door_width, features, popularity FROM estate WHERE latitude <= ? AND latitude >= ? AND longitude <= ? AND longitude >= ? ORDER BY popularity DESC, id ASC`
 	//err = db.Select(&estatesInBoundingBox, query, b.BottomRightCorner.Latitude, b.TopLeftCorner.Latitude, b.BottomRightCorner.Longitude, b.TopLeftCorner.Longitude)
